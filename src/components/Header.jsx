f9a66ff4c71a4c8699b3e90391412b188d3c490d
Aadarsha Khatri <khatriaadarsha2@gmail.com>
2024-11-13 22:28:29 +0545

    Pfp moved

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
 
         <hr className="rotate w-px h-10 bg-gray-300 mx-4" />
 
-        <div className='flex flex-row w-40 items-center'>
+        <div className='flex flex-row w-40 items-center mr-5'>
           <h5 className='w-full'>Abhi Dongol</h5>
           <img src = "pfp.png" alt = "profile"></img>
         </div>
